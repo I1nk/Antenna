707ffb19464972579f8aba58904408ad1e7fb516
I1nk <m05rob@aol.com>
2014-12-07 11:42:05 -0500

    Added comments to the code added the -vs back to the magnetic frill generator. Added cells grouping to the matlab code to seperate code sections in the script.

--- Test.m
+++ Test.m
@@ -9,13 +9,15 @@ l = lamda*lens;
 N = (l/gap) + (mod((l/gap),2) == 0);
 zvalue = zeros(1,N);
 E = zeros(1,N);
-%E(ceil(N/2)) = vs/gap;
 a = .0001;
 b = 2.3*a;
 offset = l * -0.5;
 delz = l / N;
+%This is for the delta gap generator
+%E(ceil(N/2)) = vs/gap;
 
-
+%%
+%Set up the magnetic frill generator
 for k = 1:N
     
     %find the mid point for the segment and offset it to -l/2 to l/2
@@ -39,19 +41,23 @@ for k = 1:N
     %Find E
     %      k
     E(k) = 2 * log(b/a);
-    E(k) = 1 / E(k);
-    E(k) = E(k) * ( (exp(-1j*2*pi*r1/lamda) / r1) ...
+    E(k) = vs / E(k);
+    E(k) = -E(k) * ( (exp(-1j*2*pi*r1/lamda) / r1) ...
         - (exp(-1j*2*pi*r2/lamda) / r2) );
     
 end
 
+%%
 % get current distribution (normal conditions)
 [I,z,cnd] = pfield(l/lamda,.0001/lamda,E,'e','d');
-for i = 1:N
-    I(i) = abs(I(i));
-    z(i) = z(i) * lamda;
-end
 
+%Find the magnitude of the current
+Ia = abs(I);
+%Set up the impedance 
+z = z * lamda;
+
+%%
+%plot the information
 figure;
-plot(zvalue,I)
+plot(zvalue,Ia)
 grid on
