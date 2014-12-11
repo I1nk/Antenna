2939ac38c2c3d82298c58134794fff886218b4f4
I1nk <m05rob@aol.com>
2014-12-11 15:39:46 -0500

    changed the method that the code was using to find the total impedance of the antenna. Removed the ploting of the imedance of the antenna

--- HallenTest.m
+++ HallenTest.m
@@ -45,7 +45,21 @@ end
 %imp = imp ./ I;
 
 %find the sum of the impedance of the antenna
+%Find the impedance of the antenna
+for k = 1:N
+   if(I(k) == 0)
+      imp(k) = 0;
+   else
+      imp(k) = E(k) * abs(zvalue(k));
+      imp(k) = imp(k) / I(k);
+   end
+end
 
+%for matlab only
+%imp = E .* z;
+%imp = imp ./ I;
+
+%find the sum of the impedance of the antenna
 imp_sum = sum(imp);
 
 fprintf('Z = %f %fi ohms\n',real(imp_sum),imag(imp_sum));
@@ -59,11 +73,5 @@ xlim([-l/2 l/2])
 grid on
 
 %For testing only and not going to use this plot to turn in with the report.
-figure;
-plot(zvalue,abs(imp))
-xlim([-l/2 l/2])
-grid on
-
-
 
 
